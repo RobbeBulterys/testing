0396629bf760f6c14d2d8e152a123205c4a60864
RobbeBulterys <robbe.bulterys@hogent.be>
2022-10-14 16:08:18 +0200

    test object

---
+++ Schets_ProjectWerk/Schets_ProjectWerk/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Schets_ProjectWerk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BedrijvenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            BedrijfPanel.Visibility = Visibility.Visible;
+            BedrijvenBtn.Opacity = 1;
+            WerknemersBtn.Opacity = 0.5;
+            BezoekersBtn.Opacity =  0.5;
+        }
+
+        private void WerknemersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            BedrijfPanel.Visibility = Visibility.Hidden;
+            BedrijvenBtn.Opacity = 0.5;
+            WerknemersBtn.Opacity = 1;
+            BezoekersBtn.Opacity = 0.5;
+        }
+
+        private void BezoekersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            BedrijfPanel.Visibility = Visibility.Hidden;
+            BedrijvenBtn.Opacity = 0.5;
+            WerknemersBtn.Opacity = 0.5;
+            BezoekersBtn.Opacity = 1;
+        }
+    }
+}
