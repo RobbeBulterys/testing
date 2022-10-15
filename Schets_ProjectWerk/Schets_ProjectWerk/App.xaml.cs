12a0b1c264baf251a8fcc01750ef4e8ea90ebc42
RobbeBulterys <robbe.bulterys@hotmail.com>
2022-10-15 22:55:19 +0200

    pushen

--- Schets_ProjectWerk/Schets_ProjectWerk/App.xaml.cs
+++ Schets_ProjectWerk/Schets_ProjectWerk/App.xaml.cs
@@ -13,5 +13,11 @@ namespace Schets_ProjectWerk
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.WindowState = WindowState.Maximized;
+            window.Show();
+        }
     }
 }
--- Schets_ProjectWerk/Schets_ProjectWerk/MainWindow.xaml.cs
+++ Schets_ProjectWerk/Schets_ProjectWerk/MainWindow.xaml.cs
@@ -30,58 +30,100 @@ namespace Schets_ProjectWerk
             if (sender.GetType() == typeof(Button))
             {
                 Button button = (Button)sender;
-                BedrijvenBtn.Opacity = 0.5;
-                WerknemersBtn.Opacity = 0.5;
-                BezoekersBtn.Opacity = 0.5;
-                BedrijfPanel.Visibility = Visibility.Hidden;
-                BedrijfToevoegenGridPanel.Visibility = Visibility.Hidden;
-                TopSecondDP.Background = Brushes.LightGray;
+                TopRowBtnExtras();
                 if (button.Name == "BedrijvenBtn")
                 {
                     BedrijfPanel.Visibility = Visibility.Visible;
                     TopSecondDP.Background = Brushes.Gray;
                     BedrijvenBtn.Opacity = 1;
                     BedrijfToevoegenBtn.Opacity = 1;
-                    BedrijfVerwijderenBtn.Opacity = 1;
-                    BedrijfAanpassenBtn.Opacity = 1;
+                    BedrijfOpzoekenBtn.Opacity = 1;
                     ContractToevoegenBtn.Opacity = 1;
-                    ContractVerwijderenBtn.Opacity = 1;
-                    ContractAanpassenBtn.Opacity = 1;
+                    ContractOpzoekenBtn.Opacity = 1;
                 } 
                 else if (button.Name == "WerknemersBtn")
+                {
+                    WerknemerPanel.Visibility = Visibility.Visible;
+                    TopSecondDP.Background = Brushes.Gray;
                     WerknemersBtn.Opacity = 1;
+                    WerknemerToevoegenBtn.Opacity = 1;
+                    WerknemerVerwijderenBtn.Opacity = 1;
+                    WerknemerAanpassenBtn.Opacity = 1;
+                }
                 else if (button.Name == "BezoekersBtn")
+                {
+                    BezoekerPanel.Visibility = Visibility.Visible;
+                    TopSecondDP.Background = Brushes.Gray;
                     BezoekersBtn.Opacity = 1;
+                    BezoekerToevoegenBtn.Opacity = 1;
+                    BezoekerVerwijderenBtn.Opacity = 1;
+                    BezoekerAanpassenBtn.Opacity = 1;
+                }
             }
+        }
+        private void TopRowBtnExtras()
+        {
+            BedrijvenBtn.Opacity = 0.5;
+            WerknemersBtn.Opacity = 0.5;
+            BezoekersBtn.Opacity = 0.5;
+            BedrijfPanel.Visibility = Visibility.Hidden;
+            WerknemerPanel.Visibility = Visibility.Hidden;
+            BezoekerPanel.Visibility = Visibility.Hidden;
+            BedrijfToevoegenGridPanel.Visibility = Visibility.Collapsed;
+            BedrijfOpzoekenGridPanel.Visibility = Visibility.Collapsed;
+            TopSecondDP.Background = Brushes.LightGray;
         }
         private void TopRowBedrijfBtn_Click(object sender, RoutedEventArgs e)
         {
             if (sender.GetType() == typeof(Button))
             {
                 Button button = (Button)sender;
-                BedrijfToevoegenBtn.Opacity = 0.5;
-                BedrijfVerwijderenBtn.Opacity = 0.5;
-                BedrijfAanpassenBtn.Opacity = 0.5;
-                ContractToevoegenBtn.Opacity = 0.5;
-                ContractVerwijderenBtn.Opacity = 0.5;
-                ContractAanpassenBtn.Opacity = 0.5;
-                BedrijfToevoegenGridPanel.Visibility = Visibility.Hidden;
+                TopRowBedrijfBtnExtras();
                 if (button.Name == "BedrijfToevoegenBtn")
                 {
                     BedrijfToevoegenGridPanel.Visibility = Visibility.Visible;
                     BedrijfToevoegenBtn.Opacity = 1;
                 }
-                else if (button.Name == "BedrijfVerwijderenBtn")
-                    BedrijfVerwijderenBtn.Opacity = 1;
-                else if (button.Name == "BedrijfAanpassenBtn")
-                    BedrijfAanpassenBtn.Opacity = 1;
+                else if (button.Name == "BedrijfOpzoekenBtn")
+                {
+                    BedrijfOpzoekenGridPanel.Visibility = Visibility.Visible;
+                    BedrijfOpzoekenBtn.Opacity = 1;
+                }
                 else if (button.Name == "ContractToevoegenBtn")
                     ContractToevoegenBtn.Opacity = 1;
-                else if (button.Name == "ContractVerwijderenBtn")
-                    ContractVerwijderenBtn.Opacity = 1;
-                else if (button.Name == "ContractAanpassenBtn")
-                    ContractAanpassenBtn.Opacity = 1;
+                else if (button.Name == "ContractOpzoekenBtn")
+                    ContractOpzoekenBtn.Opacity = 1;
+                else if (button.Name == "WerknemerToevoegenBtn")
+                    WerknemerToevoegenBtn.Opacity = 1;
+                else if (button.Name == "WerknemerVerwijderenBtn")
+                    WerknemerVerwijderenBtn.Opacity = 1;
+                else if (button.Name == "WerknemerAanpassenBtn")
+                    WerknemerAanpassenBtn.Opacity = 1;
+                else if (button.Name == "BezoekerToevoegenBtn")
+                    BezoekerToevoegenBtn.Opacity = 1;
+                else if (button.Name == "BezoekerVerwijderenBtn")
+                    BezoekerVerwijderenBtn.Opacity = 1;
+                else if (button.Name == "BezoekerAanpassenBtn")
+                    BezoekerAanpassenBtn.Opacity = 1;
             }
+        }
+        private void TopRowBedrijfBtnExtras()
+        {
+            BedrijfToevoegenBtn.Opacity = 0.5;
+            BedrijfOpzoekenBtn.Opacity = 0.5;
+            BedrijfToevoegenGridPanel.Visibility = Visibility.Collapsed;
+            BedrijfOpzoekenGridPanel.Visibility = Visibility.Collapsed;
+
+            ContractToevoegenBtn.Opacity = 0.5;
+            ContractOpzoekenBtn.Opacity = 0.5;
+
+            WerknemerToevoegenBtn.Opacity = 0.5;
+            WerknemerVerwijderenBtn.Opacity = 0.5;
+            WerknemerAanpassenBtn.Opacity = 0.5;
+
+            BezoekerToevoegenBtn.Opacity = 0.5;
+            BezoekerVerwijderenBtn.Opacity = 0.5;
+            BezoekerAanpassenBtn.Opacity = 0.5;
         }
     }
 }
